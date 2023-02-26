0c4e81bec000bda110d7dc4a172b8f45666ae5e1
denuwan55 <ldweerarathne5555@gmail.com>
2023-02-26 19:11:22 +0530

    patient routes "add" and "update" changed

--- routes/patient.js
+++ routes/patient.js
@@ -3,32 +3,45 @@ const router = express.Router();
 const Patient = require("../models/Patient");
 const User = require("../models/User");
 const { authenticateToken } = require("../middleware/auth");
+const { default: mongoose } = require("mongoose");
 
 // add new patient
-router.post("/add", async (req, res) => {
+router.post("/add", authenticateToken, async (req, res) => {
   try {
-    const patient = await Patient.findOne({ patient_id: req.body.patient_id });
-
-    if (patient) {
-      const others = patient._doc;
-      others["message"] = "Patient Id already exists";
-      return res.status(200).json(others);
-    } else {
-      const newPatient = new Patient({
+    const requestedClinician = await User.findOne({
+      email: req.email,
+      role: { $in: [3] },
+    });
+    if (requestedClinician) {
+      const patient = await Patient.findOne({
         patient_id: req.body.patient_id,
-        clinician_id: req.body.clinician_id,
-        name: req.body.name,
-        risk_factors: req.body.risk_factors,
-        DOB: req.body.DOB,
-        gender: req.body.gender,
-        histo_diagnosis: req.body.histo_diagnosis,
-        consent_form: req.body.consent_form,
+        clinician_id: requestedClinician._id,
       });
+      if (patient) {
+        const others = patient._doc;
+        others["message"] = "Patient Id already exists";
+        return res.status(200).json(others);
+      } else {
+        console.log(requestedClinician);
+        console.log(requestedClinician._id);
+        const newPatient = new Patient({
+          patient_id: req.body.patient_id,
+          clinician_id: requestedClinician._id,
+          name: req.body.name,
+          risk_factors: req.body.risk_factors,
+          DOB: req.body.DOB,
+          gender: req.body.gender,
+          histo_diagnosis: req.body.histo_diagnosis,
+          consent_form: req.body.consent_form,
+        });
 
-      const patient = await newPatient.save();
-      const others = patient._doc;
-      others["message"] = "Successfully added";
-      res.status(200).json(others);
+        const patient = await newPatient.save();
+        const others = patient._doc;
+        others["message"] = "Successfully added";
+        res.status(200).json(others);
+      }
+    } else {
+      res.status(401).json({ message: "Unauthorized Access!" });
     }
   } catch (error) {
     res.status(500).json(error);
@@ -39,34 +52,43 @@ router.post("/add", async (req, res) => {
 router.post("/update/:id", authenticateToken, async (req, res) => {
   try {
     const requestedClinician = await User.find({ email: req.email });
-    const patient = await Patient.find({
-      clinician_id: requestedClinician._id,
-    });
-
-    if (!patient) {
-      return res.status(401).json({ messsage: "Patient ID doesnt exists!" });
-    } else {
-      patient.name = req.body.name;
-      patient.DOB = req.body.DOB;
-      patient.risk_factors = req.body.risk_factors;
-      patient.gender = req.body.gender;
-      patient.histo_diagnosis = req.body.histo_diagnosis;
-      patient.consent_form = req.body.consent_form;
-
-      patient.save((err) => {
-        if (err) {
-          console.log(err);
-          return res.status(400).json({ messsage: err.message });
-        }
+    if (requestedClinician) {
+      const patient = await Patient.find({
+        clinician_id: requestedClinician._id,
+        patient_id: req.params.id,
       });
 
-      const updatedPatient = await Patient.findById(req.params.id);
-      const others = updatedPatient._doc;
-      others["message"] = "Successfully added";
-      res.status(200).json(others);
+      if (!patient) {
+        return res.status(401).json({ messsage: "Patient ID doesnt exists!" });
+      } else {
+        const update = await Patient.findOneAndUpdate(
+          { _id: req.params.id },
+          {
+            name: req.body.name ? req.body.name : patient.name,
+            DOB: req.body.DOB ? req.body.DOB : patient.DOB,
+            risk_factors: req.body.risk_factors
+              ? body.risk_factors
+              : patient.risk_factors,
+            gender: req.body.gender ? req.body.gender : patient.gender,
+            histo_diagnosis: req.body.histo_diagnosis
+              ? req.body.histo_diagnosis
+              : patient.histo_diagnosis,
+            consent_form: req.body.consent_form
+              ? req.body.consent_form
+              : patient.consent_form,
+          }
+        );
+
+        const updatedPatient = await Patient.findById(req.params.id);
+        const others = updatedPatient._doc;
+        others["message"] = "Successfully added";
+        res.status(200).json(others);
+      }
+    } else {
+      res.status(401).json({ message: "Unauthorized Access!" });
     }
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json(error.message);
   }
 });
 
